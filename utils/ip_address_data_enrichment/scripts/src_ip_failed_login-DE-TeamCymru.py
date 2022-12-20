7f88b5cdbba01b664e74c41f09846c001816526c
Stephen Scally <stevescally@gmail.com>
2022-12-20 10:46:17 +0000

    - Fixed missing comma that was removed when checking for whitespace - Ran pylint to fix/correct formatting and naming - Added abspath to input and output file pandas processing which   allows for accurate debug messages on file locations. - Ran bandit on script however its unclear if the configuration   is correct since no issues were found.

--- utils/ip_address_data_enrichment/scripts/src_ip_failed_login-DE-TeamCymru.py
+++ utils/ip_address_data_enrichment/scripts/src_ip_failed_login-DE-TeamCymru.py
@@ -35,185 +35,204 @@ import logging
 import argparse
 import pandas as pd
 import numpy as np
-import dns.query, dns.message, dns.reversename, dns.resolver
+import dns.query
+import dns.message
+import dns.reversename
+import dns.resolver
 from rich.progress import track
 from rich.logging import RichHandler
 
 
 def cymru_dns_q(_ip_addrs):
 
-  '''
-  Parameters
-  ----------
-  _ip_addrs:
-      List of all source IP addresses from the inital dataset.
+    '''
+    Parameters
+    ----------
+    _ip_addrs:
+        List of all source IP addresses from the inital dataset.
 
-  Returns
-  -------
-  lookup_list:
-      List with ASN, BGP Prefix, Country Code, RIR, Allocation Date, AS Description, and AS Peers.
+    Returns
+    -------
+    lookup_list:
+        List with ASN, BGP Prefix, Country Code, RIR, Allocation Date, AS Description, and AS Peers.
 
-  Notes
-  -----
-  Each IP address is queried for related information using TeamCymru
-  IP to ASN mapping service.
+    Notes
+    -----
+    Each IP address is queried for related information using TeamCymru
+    IP to ASN mapping service.
 
-  https://www.team-cymru.com/ip-asn-mapping
+    https://www.team-cymru.com/ip-asn-mapping
 
-  The DNS lookup functionality is used to query data for the respective
-  v4 or v6 IP address. Additionally, BGP peers of the specific IP address
-  are also queried.
+    The DNS lookup functionality is used to query data for the respective
+    v4 or v6 IP address. Additionally, BGP peers of the specific IP address
+    are also queried.
 
-  In order to use the DNS lookup functionality we need to perform the
-  following:
+    In order to use the DNS lookup functionality we need to perform the
+    following:
 
-  1. Loop through each IP address in the source IP address list and build an entry
-     of reversed octect v4/v6 query to the respective IPv4 and IPv6 endpoints.
+    1.  Loop through each IP address in the source IP address list and build
+        an entry of reversed octect v4/v6 query to the respective IPv4 and
+        IPv6 endpoints.
 
-     Example query v4:
-     dig +short 31.108.90.216.origin.asn.cymru.com TXT
+        Example query v4:
+        dig +short 31.108.90.216.origin.asn.cymru.com TXT
 
-     Example query v6:
-     dig +short 8.6.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.2.0.0.b.0.6.8.4.1.0.0.2.origin6.asn.cymru.com. TXT
-    
-  2. Check the return code of the query as all IP addresses may not have an entry or a lookup can timeout. 
-     We need to create a row entry regardless so that the index mapping of the dataframes is consistant.
-     This allows us to merge the dataframes later into one.
+        Example query v6:
+        dig +short 8.6.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.2.0.0.b.0.6.8.4.1.0.0.2.origin6.asn.cymru.com. TXT
 
-  3. With a successful response code we can parse the returned answer into the information we
-     want.
+    2.  Check the return code of the query as all IP addresses may not have an
+        entry or a lookup can timeout. We need to create a row entry regardless
+        so that the index mapping of the dataframes is consistant. This allows
+        us to merge the dataframes later into one.
 
-     We extract the ASN associated with the returned IP adddress query and perform a lookup
-     to the ASN domain endpoint (.asn.cymru.com)
+    3a. With a successful response code we can parse the returned answer
+        into the information we want.
 
-     We extract the list of AS peers as seen through BGP from the respective IP address.
+        We extract the ASN associated with the returned IP adddress query
+        and perform a lookup to the ASN domain endpoint (.asn.cymru.com)
 
-  4. With the inital IP address query data, AS Description and list of peer AS's a dictionary entry
-     is created for each IP address queried. The list is returned and stored as a pandas dataframe.
+        We extract the list of AS peers as seen through BGP from the
+        respective IP address.
 
-  '''
+    3b. If the response code is unsucessful we fill the values with the
+        numpy not-a-number(NaN) value. This maintains the n-dementional
+        array size between the original source and returned query data.
 
-  v4_reverse_dom = dns.name.from_text("origin.asn.cymru.com.")
-  v6_reverse_dom = dns.name.from_text("origin6.asn.cymru.com.")
-  peer_dom = dns.name.from_text("peer.asn.cymru.com.")
-  asn_dom = ".asn.cymru.com"
-  sys_resolvers = dns.resolver.get_default_resolver().nameservers
+    4.  With the inital IP address query data, AS Description and list
+        of peer AS's a dictionary entry is created for each IP address
+        queried. The list is returned and stored as a pandas dataframe.
+    '''
 
-  lookup_list = []
+    v4_reverse_dom = dns.name.from_text("origin.asn.cymru.com.")
+    v6_reverse_dom = dns.name.from_text("origin6.asn.cymru.com.")
+    peer_dom = dns.name.from_text("peer.asn.cymru.com.")
+    asn_dom = ".asn.cymru.com"
+    sys_resolvers = dns.resolver.get_default_resolver().nameservers
 
-  # Status / time tracking
-  log.info("Starting IP address DNS query")
+    lookup_list = []
 
-  for ip in track(_ip_addrs, "Querying IP Data"):
-    # IP query to get  AS number, BGP prefix, Country Code, RIR, Alocation Date
-    query_ip = dns.reversename.from_address(ip, v4_reverse_dom, v6_reverse_dom)
-    query_ip_mesg = dns.message.make_query(query_ip, dns.rdatatype.TXT)
-    query_ip_response = dns.query.udp(query_ip_mesg, sys_resolvers[0])
+    # Status / time tracking
+    LOG.info("Starting IP address DNS query")
 
-    # Check rcode.
-    if query_ip_response.rcode() == 0:
+    for ip in track(_ip_addrs, "Querying IP Data"):
+        # IP query to get  AS number, BGP prefix, Country Code, RIR, Alocation Date
+        query_ip = dns.reversename.from_address(ip, v4_reverse_dom, v6_reverse_dom)
+        query_ip_mesg = dns.message.make_query(query_ip, dns.rdatatype.TXT)
+        query_ip_response = dns.query.udp(query_ip_mesg, sys_resolvers[0])
 
-      query_ip_answer = str(query_ip_response.answer[0][0]).replace('"', "").split("|")
+        # Check rcode.
+        if query_ip_response.rcode() == 0:
 
-      # ASN query to obtain AS description
-      query_asn = 'AS' + str(query_ip_answer[0]).strip() + asn_dom
-      query_asn_mesg = dns.message.make_query(query_asn, dns.rdatatype.TXT)
-      query_asn_response = dns.query.udp(query_asn_mesg, sys_resolvers[0])
-      query_asn_answer = str(query_asn_response.answer[0][0]).replace('"', "").split("|")
+            query_ip_answer = str(query_ip_response.answer[0][0]).replace('"', "").split("|")
 
-      # Peer query
-      query_peer = dns.reversename.from_address(ip, peer_dom, peer_dom)
-      query_peer_mesg = dns.message.make_query(query_peer, dns.rdatatype.TXT)
-      query_peer_response = dns.query.udp(query_peer_mesg, sys_resolvers[0])
-      query_peer_answer = str(query_peer_response.answer[0][0]).replace('"', "").split("|")
+            # ASN query to obtain AS description
+            query_asn = 'AS' + str(query_ip_answer[0]).strip() + asn_dom
+            query_asn_mesg = dns.message.make_query(query_asn, dns.rdatatype.TXT)
+            query_asn_response = dns.query.udp(query_asn_mesg, sys_resolvers[0])
+            query_asn_answer = str(query_asn_response.answer[0][0]).replace('"', "").split("|")
 
-      # Build list of dictionaries for the response
-      # 0 - ASN, 1 - BGP Prefix, 2 - Country Code, 3 - RIR, 4 - Allocation Date, 5 - AS Description, 6 - AS Peers
-      lookup_list.append({'ASN': query_ip_answer[0].strip(), 'BGP Prefix': query_ip_answer[1].strip(), 'Country Code': query_ip_answer[2].strip(), 
-                          'RIR': query_ip_answer[3].strip(), 'Allocation Date': query_ip_answer[4].strip(), 'AS Description': query_asn_answer[4].strip(),
-                          'AS Peers': query_peer_answer[0].strip()})
-    else:
-      # NaN is used as its pandas criteria for working with the dropna functionality
-      # See https://pandas.pydata.org/docs/user_guide/missing_data.html#missing-data
-      lookup_list.append({'ASN': np.NaN, 'BGP Prefix': np.NaN, 'Country Code': 'None', 
-                          'RIR': 'None', 'Allocation Date': np.NaN, 'AS Description': 'None'
-                          'AS Peers': np.NaN})
+            # Peer query
+            query_peer = dns.reversename.from_address(ip, peer_dom, peer_dom)
+            query_peer_mesg = dns.message.make_query(query_peer, dns.rdatatype.TXT)
+            query_peer_response = dns.query.udp(query_peer_mesg, sys_resolvers[0])
+            query_peer_answer = str(query_peer_response.answer[0][0]).replace('"', "").split("|")
 
-  return lookup_list
+            # Build list of dictionaries for the response
+            # 0 - ASN, 1 - BGP Prefix, 2 - Country Code, 3 - RIR,
+            # 4 - Allocation Date, 5 - AS Description, 6 - AS Peers
+            lookup_list.append({'ASN': query_ip_answer[0].strip(),
+                                'BGP Prefix': query_ip_answer[1].strip(),
+                                'Country Code': query_ip_answer[2].strip(),
+                                'RIR': query_ip_answer[3].strip(),
+                                'Allocation Date': query_ip_answer[4].strip(),
+                                'AS Description': query_asn_answer[4].strip(),
+                                'AS Peers': query_peer_answer[0].strip()})
+
+        else:
+            # NaN is used as its pandas criteria for working with the dropna functionality
+            # See https://pandas.pydata.org/docs/user_guide/missing_data.html#missing-data
+            lookup_list.append({'ASN': np.NaN,
+                                'BGP Prefix': np.NaN,
+                                'Country Code': 'None',
+                                'RIR': 'None',
+                                'Allocation Date': np.NaN,
+                                'AS Description': 'None',
+                                'AS Peers': np.NaN})
+
+    return lookup_list
 
 
 
 def main():
 
-  # Collect user input
-  parser = argparse.ArgumentParser(
-            description="Data Enrichment (DE) of IP address datasets using TeamCymru.",
-            epilog="See http://threatlocus.com/blog/ip-address-data-enrichment-team-cymru/")
+    # Collect user input
+    parser = argparse.ArgumentParser(
+              description="Data Enrichment (DE) of IP address datasets using TeamCymru.",
+              epilog="See http://threatlocus.com/blog/ip-address-data-enrichment-team-cymru/")
 
-  parser.add_argument('--input-file', '-if',
-                      nargs='?',
-                      default=os.path.abspath("../datasets/download/src_ip_address_failed_login_attempts.txt"),
-                      help='Specifies an alternative input filename. (csv format)')
-  parser.add_argument('--output-file', '-of',
-                      nargs='?',
-                      default=os.path.abspath("../datasets/generated/src_ip_failed_login_attempts_team_cymru_ip_to_asn_mapped.csv"),
-                      help='Specifies an alternatvie output filename. (csv format)')
-  parser.add_argument('--quiet', '-q', action='store_true', default=False,
-                      help='Only outputs the query progress to TeamCymru. Overrides --debug')
+    parser.add_argument('--input-file', '-if',
+                        nargs='?',
+                        default=os.path.abspath("../datasets/download/src_ip_address_failed_login_attempts.txt"),
+                        help='Specifies an alternative input filename. (csv format)')
+    parser.add_argument('--output-file', '-of',
+                        nargs='?',
+                        default=os.path.abspath("../datasets/generated/src_ip_failed_login_attempts_team_cymru_ip_to_asn_mapped.csv"),
+                        help='Specifies an alternatvie output filename. (csv format)')
+    parser.add_argument('--quiet', '-q', action='store_true', default=False,
+                        help='Only outputs the query progress to TeamCymru. Overrides --debug')
 
-  parser.add_argument('--debug', '-d', action='store_true',
-                      help='Enable debug logging output.')
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help='Enable debug logging output.')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  # Logging Handler
-  global log
-  FORMAT = "%(asctime)s - %(message)s"
-  LOGLEVEL=logging.INFO
+    # Logging Handler
+    global LOG
+    log_format = "%(asctime)s - %(message)s"
+    log_level=logging.INFO
 
-  if args.debug and not args.quiet:
-      LOGLEVEL=logging.DEBUG
+    if args.debug and not args.quiet:
+        log_level=logging.DEBUG
 
-  if args.quiet:
-      LOGLEVEL=logging.WARNING
+    if args.quiet:
+        log_level=logging.WARNING
 
-  logging.basicConfig(level=LOGLEVEL,format=FORMAT, datefmt=None, handlers=[RichHandler(show_time=False)])
-  log = logging.getLogger("rich")
-  log.debug("Global logger configured.")
-  log.debug(log)
+    logging.basicConfig(level=log_level,format=log_format, datefmt=None, handlers=[RichHandler(show_time=False)])
+    LOG = logging.getLogger("rich")
+    LOG.debug("Global logger configured.")
+    LOG.debug(LOG)
 
-  # Verify provided arguments
-  log.debug('Provided commandline arguments: %s', args)
+    # Verify provided arguments
+    LOG.debug('Provided commandline arguments: %s', args)
 
-  # This is our source of questionable IP addresses
-  log.info("Reading source dataset, creating dataframe.")
-  log.debug("Input file is: %s", os.path.abspath(args.input_file))
-  df_ip_address = pd.read_csv(args.input_file,
-                              delim_whitespace=1,
-                              names=['ip_count','src_ip'])
+    # This is our source of questionable IP addresses
+    LOG.info("Reading source dataset, creating dataframe.")
+    LOG.debug("Input file is: %s", os.path.abspath(args.input_file))
+    df_ip_address = pd.read_csv(os.path.abspath(args.input_file),
+                                delim_whitespace=1,
+                                names=['ip_count','src_ip'])
 
-  # Extract ip_address column into list
-  log.info("Create src_ip list from dataframe.")
-  ip_addr_lst = df_ip_address["src_ip"].tolist()
+    # Extract ip_address column into list
+    LOG.info("Create src_ip list from dataframe.")
+    ip_addr_lst = df_ip_address["src_ip"].tolist()
 
-  # Dataframe of queried data
-  log.info("Call TeamCymru lookup function.")
-  df_ip_address_data = pd.DataFrame(cymru_dns_q(ip_addr_lst))
+    # Dataframe of queried data
+    LOG.info("Call TeamCymru lookup function.")
+    df_ip_address_data = pd.DataFrame(cymru_dns_q(ip_addr_lst))
 
-  # Status / time tracking
-  log.info("Finished IP address DNS query")
+    # Status / time tracking
+    LOG.info("Finished IP address DNS query")
 
-  # Concatnate datasets
-  log.info("Concatnate both datasets")
-  all_data = pd.concat([df_ip_address,df_ip_address_data], axis=1)
+    # Concatnate datasets
+    LOG.info("Concatnate both datasets")
+    all_data = pd.concat([df_ip_address,df_ip_address_data], axis=1)
 
-  ## Generate Enriched Dataset
-  log.info("Writing dataset to new file.")
-  log.debug("Output file is: %s", os.path.abspath(args.output_file))
-  all_data.to_csv(args.output_file,
-                  index=False)
+    ## Generate Enriched Dataset
+    LOG.info("Writing dataset to new file.")
+    LOG.debug("Output file is: %s", os.path.abspath(args.output_file))
+    all_data.to_csv(os.path.abspath(args.output_file),
+                    index=False)
 
 
 if __name__ == "__main__":
     main()
-
